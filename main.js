7200266f922fbb2ddaefc9c6d9d08d9b578949dd
Marcelina <marcelinasiwecka@interia.pl>
2020-04-05 22:59:46 +0200

    first commit

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ const empties = document.querySelectorAll(".empty");
 const selectList = document.querySelectorAll(".selectList");
 const button = document.querySelector("button");
 const emptyTask = document.querySelector(".emptytask");
+const input = document.querySelector(".create-task-input");
+const buttonDone = document.querySelectorAll(".button-done");
+const buttonRemove = document.querySelectorAll(".button-remove");
+
+for (const button of buttonRemove) {
+  button.addEventListener("click", function () {
+    button.parentElement.firstElementChild.firstElementChild.remove();
+    button.parentElement.firstElementChild.className = "empty";
+  });
+}
+
+for (const button of buttonDone) {
+  button.addEventListener("click", function () {
+    button.parentElement.firstElementChild.classList.toggle("done");
+  });
+}
 
 function Select() {
   for (const option of selectList)
@@ -20,23 +36,19 @@ function Select() {
       }
     });
 
-  const input = document.querySelector(".create-task-input");
-
   input.addEventListener("input", addTodiv);
+}
 
+function addTodiv(event) {
   const par = emptyTask.children[0].children[0];
-  console.log(par);
-
-  function addTodiv(event) {
-    const value = event.target.value;
-    console.log(par, "functionaddTOdiv");
-    par.innerText = value;
-  }
+  const value = event.target.value;
+  par.innerText = value;
 }
 
 const fill = document.getElementsByClassName("fill");
 
 button.addEventListener("click", function () {
+  button.disabled = true;
   const div = document.createElement("div");
   const p = document.createElement("p");
   div.className = "fill";
@@ -49,6 +61,11 @@ button.addEventListener("click", function () {
   Select();
 
   const fill = document.querySelectorAll(".fill");
+
+  // if (fill.length > 2){
+  //   alert("hahah")
+
+  // }
 
   for (const element of fill) {
     element.addEventListener("dragstart", dragStart);
@@ -76,23 +93,46 @@ function dragEnd(event) {
 
 function dragOver(e) {
   e.preventDefault();
+  e.stopImmediatePropagation();
+
+  if (event.target.childNodes.length > 0) {
+    event.target.style.pointerEvents = "none";
+  }
 }
 
 function dragEnter(e) {
   e.preventDefault();
-  event.target.className += " hovered";
 }
 
-function dragLeave(event) {
-  event.target.className = "empty";
+function dragLeave(e) {
+  setTimeout(Levave, 1000);
+
+  function Levave() {
+    for (const element of fill) {
+      element.style.pointerEvents = "auto";
+    }
+  }
 }
 
 function dragDrop(event) {
   event.target.className = "empty";
 
+  input.value = "";
+
+  if (fill.length <= 8) {
+    button.disabled = false;
+  }
+
   for (const element of fill) {
-    if (element.className == "fill hold") {
+    if (element.className === "fill hold") {
       event.target.append(element);
+      element.style.pointerEvents = "auto";
+    } else {
+      element.style.pointerEvents = "auto";
     }
+  }
+
+  for (const empty of empties) {
+    empty.style.pointerEvents = "auto";
   }
 }
