35ebb99984227d90a996688f6384d4756d528348
Marcelina <marcelinasiwecka@interia.pl>
2020-04-07 20:06:09 +0200

    add local Storage

--- main.js
+++ main.js
@@ -6,19 +6,28 @@ const input = document.querySelector(".create-task-input");
 const buttonDone = document.querySelectorAll(".button-done");
 const buttonRemove = document.querySelectorAll(".button-remove");
 
+window.onload = function () {
+  StorageGetItem();
+};
+
+// Remove Task Button
 for (const button of buttonRemove) {
   button.addEventListener("click", function () {
     button.parentElement.firstElementChild.firstElementChild.remove();
     button.parentElement.firstElementChild.className = "empty";
+
+    StorageSetItem()
   });
 }
 
+// Change task to done
 for (const button of buttonDone) {
   button.addEventListener("click", function () {
     button.parentElement.firstElementChild.classList.toggle("done");
   });
 }
 
+// Select urgency and importance
 
 function Select() {
   for (const option of selectList)
@@ -39,6 +48,7 @@ function Select() {
 
   input.addEventListener("input", addTodiv);
 }
+// function added text to task
 
 function addTodiv(event) {
   const par = emptyTask.children[0].children[0];
@@ -48,10 +58,12 @@ function addTodiv(event) {
 
 const fill = document.getElementsByClassName("fill");
 
+// create task 
+
 button.addEventListener("click", function () {
   button.disabled = true;
 
-  //create task div and paragraf
+  //create div and paragraf
   const div = document.createElement("div");
   const p = document.createElement("p");
   div.className = "fill";
@@ -62,18 +74,19 @@ button.addEventListener("click", function () {
   div.appendChild(p);
   emptyTask.appendChild(div);
 
+  // added select for urgency and importance
   Select();
 
   const fill = document.querySelectorAll(".fill");
 
-  // add eventListener to created task div
+  // add drag event to created task div
   for (const element of fill) {
     element.addEventListener("dragstart", dragStart);
     element.addEventListener("dragend", dragEnd);
   }
 });
 
-// Add eventListener to empty box
+// Add drag event to empty box
 
 for (const empty of empties) {
   empty.addEventListener("dragover", dragOver);
@@ -82,7 +95,7 @@ for (const empty of empties) {
   empty.addEventListener("drop", dragDrop);
 }
 
-// Drag 
+// Drag function
 
 function dragStart(event) {
   event.target.className += " hold";
@@ -108,8 +121,6 @@ function dragEnter(event) {
 function dragLeave(event) {
   setTimeout(Leave, 1000);
 
-  event.target.style.backgroundColor = "white";
-
   function Leave() {
     for (const element of fill) {
       element.style.pointerEvents = "auto";
@@ -128,14 +139,61 @@ function dragDrop(event) {
 
   for (const element of fill) {
     if (element.className === "fill hold") {
+      const id = event.target.id;
+      element.setAttribute("id", id);
       event.target.append(element);
+      element.className = "fill";
+      console.log(element.id);
+
       element.style.pointerEvents = "auto";
     } else {
       element.style.pointerEvents = "auto";
     }
+
+    for (const empty of empties) {
+      empty.style.pointerEvents = "auto";
+    }
   }
 
+  StorageSetItem()
+
+}
+
+// function for get item from Storage
+function StorageGetItem() {
+  var parser = new DOMParser();
+
+  const localStoraget = JSON.parse(localStorage.getItem("obj"));
+
+  var htmlDoc = parser.parseFromString(localStoraget, "text/html");
+  const a = htmlDoc.getElementsByClassName("fill");
+
   for (const empty of empties) {
-    empty.style.pointerEvents = "auto";
+    for (const element of a) {
+      if (empty.id === element.id) {
+        empty.append(element);
+      }
+    }
   }
+}
+
+// function for set item in Storage
+function StorageSetItem(){
+
+  const drophistory = [];
+  for (const empty of empties) {
+    console.log([...fill].find((element) => element.id === empty.id));
+
+    let StorageElement = [...fill].find((element) => element.id === empty.id);
+
+    console.log(StorageElement, "storageElement");
+
+    if (StorageElement !== undefined) {
+      drophistory.push(StorageElement.outerHTML);
+    }
+  }
+
+  localStorage.setItem("obj", JSON.stringify(drophistory));
+  console.log(localStorage, "local");
+
 }
